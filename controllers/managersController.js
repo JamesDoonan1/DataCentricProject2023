e29633246e6ecc04314ca0d5d340e1122a8e0ec9
JamesDoonan1 <g00310428@atu.ie>
2023-12-16 17:48:42 +0000

    Manager Form is adding. need to check valiation

--- controllers/managersController.js
+++ controllers/managersController.js
@@ -3,7 +3,6 @@ const dao = require('../models/dao'); // Adjust the path accordingly
 
 async function getManagersPage(req, res) {
   try {
-    // Ensure that the MongoDB client is connected before proceeding
     const managersCollection = await dao.getManagersCollection();
     const managers = await managersCollection.find().toArray();
     res.render('managers', { managers });
@@ -13,7 +12,27 @@ async function getManagersPage(req, res) {
   }
 }
 
+// Function to add a manager
+async function addManager(req, res) {
+  try {
+    const managersCollection = await dao.getManagersCollection();
+
+    // Assuming you have form fields like 'name' and 'salary'
+    const { _id, name, salary } = req.body;
+
+    // Insert the new manager into the collection
+    await managersCollection.insertOne({ _id, name, salary });
+
+    // Redirect back to the same page after adding a manager
+    res.redirect('/managers');
+  } catch (error) {
+    console.error('Error adding manager:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
 module.exports = {
   getManagersPage,
+  addManager,
   // Add other exported functions here
 };
--- routes/managers.js
+++ routes/managers.js
@@ -1,8 +1,14 @@
 // routes/managers.js
 const express = require('express');
 const router = express.Router();
+
+// Import your controllers
 const managersController = require('../controllers/managersController');
 
+// Managers Page (GET)
 router.get('/managers', managersController.getManagersPage);
+
+// Handle form submission to add a manager (POST)
+router.post('/managers', managersController.addManager);
 
 module.exports = router;
